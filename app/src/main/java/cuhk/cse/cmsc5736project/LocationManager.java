dc95e261a2ef5f627cab8db1eb81ed1cbd7fcda7
Tom Chau <chy111126@gmail.com>
2017-12-18 22:15:53 +0800

    Add interface for BLE location service

--- app/src/main/java/cuhk/cse/cmsc5736project/LocationManager.java
+++ app/src/main/java/cuhk/cse/cmsc5736project/LocationManager.java
@@ -2,6 +2,9 @@ package cuhk.cse.cmsc5736project;
 
 import android.content.Context;
 
+import com.kosalgeek.genasync12.AsyncResponse;
+import com.kosalgeek.genasync12.PostResponseAsyncTask;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -30,6 +33,10 @@ public class LocationManager {
     // ----- Singleton class -----
     private static LocationManager instance;
 
+    // ----- URLs -----
+    private static String ROOT_URL = "http://218.191.44.226/cmsc5736_project";
+    private static String GET_ALL_BEACON_DATA_URL = ROOT_URL + "/get_all_beacon_data.php";
+
     // Constructor
     public static LocationManager getInstance() {
         if(instance == null) {
@@ -54,10 +61,30 @@ public class LocationManager {
     // TODO: End
 
     // POI methods
+    public void getPOIDefinitions(Context context, final OnPOIResultListener initListener) {
+        // Get POI definitions from server, and materialize for client upgrades to each approximation
+        // ~= RSSIModel.updateModel method
+        HashMap postData = new HashMap();
+        PostResponseAsyncTask task = new PostResponseAsyncTask(context, postData, new AsyncResponse() {
+            @Override
+            public void processFinish(String s) {
+                // TODO: process response to List of model objects
+
+                initListener.onRetrieved(poiBeacons);
+            }
+        });
+        task.execute(GET_ALL_BEACON_DATA_URL);
+    }
+
     public void setOnPOIResultListener(OnPOIResultListener listener) {
         // Caller method can update through the listener when this manager class sent updates
         // this.poiListener.OnRetrieved method would be invoked after service successfully acquired new location information
         this.poiListener = listener;
+    }
+
+    // Friend methods
+    public void getFriendDefinitions() {
+        // TODO: Get friend definitions from server, and materialize for client upgrades to each approximation
     }
 
     public void setOnFriendResultListener(OnFriendResultListener listener) {
