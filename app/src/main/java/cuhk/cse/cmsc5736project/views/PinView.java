74d8e5bb2fb9f4816e0c8555b39b850177a22120
Chikari <charmyhon@hotmail.com>
2017-12-26 01:30:17 +0800

    show pin

--- app/src/main/java/cuhk/cse/cmsc5736project/views/PinView.java
+++ app/src/main/java/cuhk/cse/cmsc5736project/views/PinView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PointF;
 import android.util.AttributeSet;
@@ -36,11 +37,54 @@ public class PinView extends SubsamplingScaleImageView {
     public void addPinList(List<Pin> pinList) {
         //this.sPin = new PointF(10 + 70,0 + 500);
 
-        this.pinList.addAll(pinList);
+        for (Pin pin : pinList) {
+            // modify pin location before adding
+            PointF tarPin = new PointF();
+
+            viewToSourceCoord(pin.getPin(), tarPin);
+            Log.i("pin view", "changing pin: " + pin.getPin().x + ", " + pin.getPin().y + ", " + tarPin.x + ", " + tarPin.y + ", " + getScale());
+            pin.setPin(tarPin);
+            this.pinList.add(pin);
+        }
+        //this.pinList.addAll(pinList);
 
         //initialise();
         invalidate();
     }
+
+    public void addPin(Pin pin) {
+
+        // modify pin location before adding
+        PointF tarPin = new PointF();
+
+        viewToSourceCoord(pin.getPin(), tarPin);
+        Log.i("pin view", "changing pin: " + pin.getPin().x + ", " + pin.getPin().y + ", " + tarPin.x + ", " + tarPin.y + ", " + getScale());
+        pin.setPin(tarPin);
+        this.pinList.add(pin);
+
+        //initialise();
+        invalidate();
+    }
+
+    void toPinViewCoord(Pin pin){
+        pin.setPin(viewToSourceCoord(pin.getPin()));
+        //return pin;
+    }
+
+/*    PointF viewToSourceCoord(PointF sPos){
+        PointF afterTrans = new PointF();
+        PointF translate = new PointF();
+        PointF targetPin = new PointF();
+        float scale = getScale();
+        sourceToViewCoord(sPos, afterTrans);
+        float sx = sPos.x;
+        float sy = sPos.y;
+        translate.set(afterTrans.x - sx * scale, afterTrans.y - sy * scale);
+        targetPin.set((sx - translate.x), (sy - translate.y));
+        if (scale!=0f) targetPin.set(targetPin.x/scale, targetPin.y/scale);
+
+        return targetPin;
+    }*/
 
 /*    private void initialise() {
         float density = getResources().getDisplayMetrics().densityDpi;
@@ -67,10 +111,17 @@ public class PinView extends SubsamplingScaleImageView {
             sourceToViewCoord(pin.getPin(), vPin);
             float vX = vPin.x - (pin.getBitmap().getWidth()/2);
             float vY = vPin.y - pin.getBitmap().getHeight();
-            //float vX = sPin.x;
-            //float vY = sPin.y;
             canvas.drawBitmap(pin.getBitmap(), vX, vY, paint);
-            Log.i("pin view", "onDraw: " + vX + ", " + vY + ", " + pin.getPin().x + ", " + pin.getPin().y + ", " + getScale());
+
+            if (!pin.getDescription().isEmpty()) {
+                paint.setColor(Color.BLACK);
+                paint.setTextSize(50);
+                //float vXText = vPin.x - (pin.getBitmap().getWidth() / 2);
+                //float vYText = vPin.y - pin.getBitmap().getHeight() - 20;
+                canvas.drawText(pin.getDescription(), vX, vY, paint);
+            }
+
+            //Log.i("pin view", "onDraw: " + vX + ", " + vY + ", " + pin.getPin().x + ", " + pin.getPin().y + ", " + getScale());
         }
 
     }
